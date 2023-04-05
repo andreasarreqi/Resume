555df3dd5b5a77b4639976abdcd4b3b3e4b7f239
Andre <sarreqiandrea@gmail.com>
2023-04-05 20:24:45 +0000

    Implement emailJS integration on the contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+
+    emailjs.send("gmail", "rosie", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        }, "bv55Uq_Vz0ej3_2RE")
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
+}
